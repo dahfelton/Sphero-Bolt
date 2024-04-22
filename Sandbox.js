e843783d5922fb4ec314af5cb37c8be5a6c62c51
Felton <dahfelton@gmail.com>
2024-04-22 14:12:20 -0400

    Added Sound Effect for Sphero Bolt

--- Sandbox.js
+++ Sandbox.js
@@ -4,13 +4,14 @@
 //Date: 4.16.2024
 //Program: Hello World - Sandbox
 //Program URL: https://edu.sphero.com/program/16700411/edit
+
 async function startProgram() {
 	setMainLed({ r: 0, g: 0, b: 255 });
 	await speak("Hello Square", true);
 	await delay(1);
 	for (var _i1 = 0; _i1 < 4; _i1++) {
 		setMainLed(getRandomColor());
-		await Sound.Game.Coin.play(true);
+		await Sound.Effects.Laser.play(true);
 		await roll((getHeading() + 90), 60, 1);
 		await delay(1);
 	}
