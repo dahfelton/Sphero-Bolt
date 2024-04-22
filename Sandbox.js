6273dc9a08d0fbf540f6c5762f41dc89c789e05b
Felton <dahfelton@gmail.com>
2024-04-22 13:56:57 -0400

    Added Hello Square Script from Wiki (FIXED)

--- Sandbox.js
+++ Sandbox.js
@@ -4,8 +4,8 @@
 //Date: 4.16.2024
 //Program: Hello World - Sandbox
 //Program URL: https://edu.sphero.com/program/16700411/edit
-
-setMainLed({ r: 0, g: 0, b: 255 });
+async function startProgram() {
+	setMainLed({ r: 0, g: 0, b: 255 });
 	await speak("Hello Square", true);
 	await delay(1);
 	for (var _i1 = 0; _i1 < 4; _i1++) {
@@ -13,4 +13,5 @@ setMainLed({ r: 0, g: 0, b: 255 });
 		await Sound.Game.Coin.play(true);
 		await roll((getHeading() + 90), 60, 1);
 		await delay(1);
+	}
 }
