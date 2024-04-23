a3377d48001598e7d3dfc64ab44146ae0c889b36
Felton <dahfelton@gmail.com>
2024-04-23 13:28:27 -0400

    Go straight and turn around

--- Sandbox.js
+++ Sandbox.js
@@ -5,6 +5,14 @@
 //Program: Hello World - Sandbox
 //Program URL: https://edu.sphero.com/program/16700411/edit
 
+//async function startProgram() {
+	//await speak("Hello World", true);
+	//setMainLed({ r: 0, g: 0, b: 255});
+	//setSpeed(60);
+	//await delay (2);
+	//setSpeed(0);
+//}
+/*
 async function startProgram() {
 	setMainLed({ r: 255, g: 0, b: 255 });
 	await Sound.Effects.Laser.play(true);
@@ -15,4 +23,10 @@ async function startProgram() {
 		await roll((getHeading() + 90), 60, 1);
 		await delay(1);
 	}
+}
+*/
+async function startProgram() {
+	await roll(0, 40, 2)
+	await 2
+	await roll (-180, 40, 2)
 }
