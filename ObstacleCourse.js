895084e624e21a6a5fa6366a3cd7538b6e4eb892
Felton <dahfelton@gmail.com>
2024-04-24 13:54:57 -0400

    ObstacleCourse 1st Checkpoint

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -2,5 +2,10 @@
 //Team Member: Miles Shannon
 //Bolt ID: SB-52F9
 //Date: 4.23.2024
-//Program: My Mini Course - Sandbox
+//Program: Obstacle Course - Sandbox
 //Program URL: https://edu.sphero.com/program/16752553/edit
+
+async function startProgram() {
+	await roll( 80, 80, 2),
+	await delay(1);
+}
