2b94f1f2306929ed5e3a49be111738bd782e145c
Felton <dahfelton@gmail.com>
2024-04-30 13:28:18 -0400

    Added Beginning Comments & Start Program JS

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+//Programmer: Billy Felton
+//Team Member: Miles Shannon
+//Bolt ID: SB-52F9
+//Date: 4.23.2024
+//Program: Maze Competition
+//Program URL: https://edu.sphero.com/program/16788325/edit
+
+async function startProgram() {
+	// Write code here
+}
